29839bae17b8bd3f46cab3fd1af08cd9201ee379
Viktor <v_kozintcev@mail.ru>
2024-09-09 17:25:31 +0300

    task-1

--- src/block-1_task-1.test.js
+++ src/block-1_task-1.test.js
@@ -10,7 +10,6 @@ describe("Тестирование функций суммирования и у
   it("Проверка умножения двух чисел", () => {
     expect(multiply(2, 3)).toBe(6);
     expect(multiply(-1, 1)).toBe(-1);
-    expect(multiply(0, 5)).toBe(0);
     expect(multiply(-1, -1)).toBe(1);
   });
 });
