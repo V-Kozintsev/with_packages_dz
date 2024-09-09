d01df103513e1c5c48019e583da02f85ed1def1a
Viktor <v_kozintcev@mail.ru>
2024-09-09 17:05:23 +0300

    add npm

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginJest from "eslint-plugin-jest"; // Убедитесь, что этот пакет установлен
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        it: "readonly",
+        describe: "readonly",
+        expect: "readonly",
+        module: "writable",
+      },
+    },
+    plugins: {
+      jest: pluginJest, // Подключаем плагин Jest
+    },
+    rules: {
+      "prefer-const": "warn", // Предпочитаем const
+      "no-irregular-whitespace": "error", // Запрещаем нерегулярные пробелы
+    },
+  },
+  eslintConfigPrettier,
+  pluginJs.configs.recommended,
+];
