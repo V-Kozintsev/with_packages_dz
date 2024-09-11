450ff3d14da9530709083cfead8e7fbc338ddbbd
V-Kozintsev <v_kozintcev@mail.ru>
2024-09-11 21:40:44 +0300

    task-11

--- src/block-2_task-11.js
+++ src/block-2_task-11.js
@@ -3,7 +3,9 @@ const arrayNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 export function sumNumber(sum) {
   const sumResult = sum.reduce((acc, num) => acc + num, 0);
   console.log(sumResult);
-  return sumResult;
+
+  const newArrayNumber = arrayNumber.map((num) => num * 2);
+  return { sumResult, newArrayNumber };
 }
 
 export { arrayNumber };
