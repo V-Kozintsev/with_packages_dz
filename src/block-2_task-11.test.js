0dc47508abbe799f8b84a7b0052ff2a32b6b53de
V-Kozintsev <v_kozintcev@mail.ru>
2024-09-11 21:43:04 +0300

    task-11 переделал

--- src/block-2_task-11.test.js
+++ src/block-2_task-11.test.js
@@ -1,19 +1,23 @@
 import { sumNumber, arrayNumber } from "./block-2_task-11";
 
 describe("arrayNumber", () => {
+  let result;
+
   beforeEach(() => {
     jest.spyOn(console, "log").mockImplementation();
+    result = sumNumber(arrayNumber); // Получаем результат один раз
   });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
+
   it("Выведите в консоль сумму всех элементов массива.", () => {
-    const result = sumNumber(arrayNumber);
     expect(result.sumResult).toBe(55);
     expect(console.log).toHaveBeenCalledWith(55);
   });
-  it(".Создайте новый массив на основе исходного, в котором каждый элемент будет вдвое больше элемента исходного массива с таким же индексом.(a[1] = 3, b[1] = 6, где a — исходный массив, b — новый массив)", () => {
-    const result = sumNumber(arrayNumber);
+
+  it("Создайте новый массив на основе исходного, в котором каждый элемент будет вдвое больше элемента исходного массива с таким же индексом.", () => {
     expect(result.newArrayNumber).toEqual([2, 4, 6, 8, 10, 12, 14, 16, 18, 20]);
   });
 });
